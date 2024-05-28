9a49ec58f74c6edd1273c1db6396206286cacce7
Markus <markblake190399@gmail.com>
2024-05-28 23:00:27 +0200

    frontend v6

--- js/index.js
+++ js/index.js
@@ -3,18 +3,14 @@ const contproductos = $.querySelector('#contproductos')
 
 const Cardnew = ({ title, image, description, price }) => {
     return `
-        <div class="col-12 col-md-6 col-lg-4 mb-3">
-            <div class="card text-white text-center bg-dark pb-2 animation-slide-in">
-                <div class="card-body">
+                <div class="card">
                     <h3 class="card-title">${title}</h3>
                     <img class="card-img" src="${image}" alt="">
                     <p class="card-desc">${description}</p>
                     <strong class="card-price">$ ${price}</strong>
                     <button class="btn btn-primary">agregar al carrito</button>
                 </div>
-            </div>
-        </div>
-    `
+        `
 }
 
 const renderCards = (array) => {
