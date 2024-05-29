d009a0527d632673b3c9af18e4c428a5ef5611a2
Markus <markblake190399@gmail.com>
2024-05-29 11:56:36 +0200

    frontend v7.1

--- js/index.js
+++ js/index.js
@@ -3,20 +3,20 @@ const contproductos = $.querySelector('#contproductos')
 
 const Cardnew = ({ title, image, description, price }) => {
     return `
-                <div class="card">
-                    <h3 class="card-title">${title}</h3>
-                    <img class="card-img" src="${image}" alt="">
-                    <p class="card-desc">${description}</p>
-                    <strong class="card-price">$ ${price}</strong>
-                    <button class="btn btn-primary">agregar al carrito</button>
-                </div>
+    <div class="card">
+    <h3 class="card-title">${title}</h3>
+    <img class="card-img" src=${image} alt="">
+    <p class="card-desc">${description}</p>
+    <strong class="card-price">$ ${price}</strong>
+    <button class="btn btn-primary">Agregar al Carrito</button>
+  </div>
         `
 }
 
 const getAll = async () => {
     try {
         const response = await fetch('http://localhost:3007/api/productos')
-        if (response.status !== 200) throw new Error ('error en la peticion')
+        if (response.status !== 200) throw new Error('error en la peticion')
         const data = await response.json()
         renderCards(data)
     } catch (error) {
