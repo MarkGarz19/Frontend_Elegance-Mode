e6aae6d9b236b1ae38a47d859f90968fd0192cf9
Markus <markblake190399@gmail.com>
2024-06-17 10:06:02 +0200

    frontend v20.1

--- js/index.js
+++ js/index.js
@@ -14,7 +14,13 @@ const Cardnew = ({ id, title, image, description, price }) => { // se creara la 
 // CODIGO PARA EJECUTAR LA PAGINA DESPLEGADA
 const getAll = async () => { // esta funcion asicronica deberia comunicarse con la base de datos para obtener todos los productos
     try {
-        const response = await fetch(`https://backend-elegance-mode.onrender.com/api/productos`) // esta peticion es para obtener todos los productos de la base de datos a traves del render
+        const response = await fetch(`https://backend-elegance-mode.onrender.com/api/productos`, {
+            method: 'GET',
+            mode: 'cors',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }) // esta peticion es para obtener todos los productos de la base de datos a traves del render
         if (response.status !== 200) throw new Error('error en la peticion') // si la peticion no es exitosa daria un error
         const data = await response.json() // se obtiene el producto y se convierte en un objeto JSON
         renderCards(data) // luego se lo mandara a la funcion para renderizar la card
