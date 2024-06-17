e960169fdce5cc030c64b0c856cb7ef347ad81fb
Markus <markblake190399@gmail.com>
2024-06-17 12:29:48 +0200

    frontend v23.1

--- js/index.js
+++ js/index.js
@@ -14,13 +14,7 @@ const Cardnew = ({ id, title, image, description, price }) => { // se creara la 
 // CODIGO PARA EJECUTAR LA PAGINA DESPLEGADA
 const getAll = async () => { // esta funcion asicronica deberia comunicarse con la base de datos para obtener todos los productos
     try {
-        const response = await fetch(`https://backend-elegance-mode.onrender.com/api/productos`, {
-            method: 'GET',
-            mode: 'cors',
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        }) // esta peticion es para obtener todos los productos de la base de datos a traves del render
+        const response = await fetch(`https://backend-elegance-mode.onrender.com/api/productos`) // esta peticion es para obtener todos los productos de la base de datos a traves del render
         if (response.status !== 200) throw new Error('error en la peticion') // si la peticion no es exitosa daria un error
         const data = await response.json() // se obtiene el producto y se convierte en un objeto JSON
         renderCards(data) // luego se lo mandara a la funcion para renderizar la card
@@ -60,7 +54,7 @@ const EventoBotonCarrito = () => { //esta funcion es para los botones
 
 const agregarcarrito = (e) => { // esta funcion es para agregar los productos al carrito
     const idproducto = e.target.getAttribute('data-product'); // se obtiene el id del producto a traves del data-product
-    fetch(`https://backend-elegance-mode.onrender.com/api/productos/carrito/${idproducto}`,) // esta peticion es para agregar el producto al carrito a la base de datos a traves del render
+    fetch(`https://backend-elegance-mode.onrender.com/api/productos/carrito/${idproducto}`) // esta peticion es para agregar el producto al carrito a la base de datos a traves del render
         .then(res => res.json())
         .then(json => {
             let carrito = JSON.parse(localStorage.getItem('carrito')) || []; // se guardara el carrito en el local storage o se inicializa vacio
